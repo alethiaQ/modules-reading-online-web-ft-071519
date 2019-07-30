d05dbe1603678aa4f5b7b72bad51a1426047f253
Alethia Quintero <alethiaq22@gmail.com>
2019-07-30 23:17:39 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -18,8 +18,8 @@ module FancyDance
     end
   end
 
-  module ClassMethods 
-    def metadata 
+  module ClassMethods
+    def metadata
       "This class produces objects that love to dance."
     end
   end
